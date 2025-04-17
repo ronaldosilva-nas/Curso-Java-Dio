38a8a0891cad0444fa52ca913d89cc6266c66699
ronaldosilva-nas <silvaronaldo7662@gmail.com>
2025-04-17 11:08:14 -0300

    Aula de estrutura de repetição
    Estrutura de repetição - For

---
+++ estrutura-de-repeticao/src/ExemploFor.java
@@ -0,0 +1,10 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        
+        //estrutura de repetição - For
+
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos ++) {
+            System.out.println("Contando carneirinhos " + carneirinhos);
+        }
+    }
+}
